e47029af9875cc40d466f7202f83e2103aa6d27a
Fushimi <kosuke-fushimi@smilead.co.jp>
2022-01-02 12:56:00 +0900

    styled sidemenu

--- src/renderer/components/Navigation.vue
+++ src/renderer/components/Navigation.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="navigation flex flex-col items-start">
+    <div class="flex flex-col items-start" id="navigation">
         <!-- Home Menu -->
-        <router-link to="/" class="flex items-center mb-3">
+        <router-link to="/" class="flex items-center mb-3 w-full">
             <fa icon="phone" class="fa-icon" />
             <p class="pl-3">Home</p>
         </router-link>
 
-        <!-- Search Menu -->
-        <router-link to="/search" class="flex items-center mb-3">
-            <fa icon="search" class="fa-icon" />
-            <p class="pl-3">Search</p>
-        </router-link>
+        <!-- Search Accordion menu -->
+        <div class="accordion mb-3">
+            <div class="accordion-item">
+                <h3 class="title">
+                    <fa icon="search" class="fa-icon" />
+                    <p class="pl-3 mr-auto">Search</p>
+                    <fa icon="chevron-down" class="fa-icon arrow" />
+                </h3>
+
+                <!-- Form contents -->
+                <div class="accordion-contents">
+                    <p>Form will be didplayed here</p>
+                </div>
+            </div>
+        </div>
 
         <!-- News Menu -->
-        <router-link to="/news" class="flex items-center mb-3">
+        <router-link to="/news" class="flex items-center mb-3 w-full">
             <fa icon="newspaper" class="fa-icon" />
             <p class="pl-3">News</p>
         </router-link>
@@ -21,24 +31,95 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 
 export default defineComponent({
 	setup() {
+		onMounted(() => {
+			// アコーディオンイベント追加
+			const accordionItem = document.querySelectorAll('.accordion-item')
+			accordionItem.forEach((el) =>
+				el.addEventListener('click', () => {
+					if (el.classList.contains('active')) {
+						el.classList.remove('active')
+					} else {
+						accordionItem.forEach((el2) => el2.classList.remove('active'))
+						el.classList.add('active')
+					}
+				})
+			)
+		})
 
 	},
 })
 </script>
 
 <style lang="scss" scoped>
-.navigation {
+#navigation {
     height: 100%;
-    color: $accent;
+    padding: 5px;
+    color: $gray;
+    background-color: #161c2e;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+        display: none;
+    }
     a {
         &.router-link-exact-active {
           color: aqua;
             .fa-icon {
                 color: aqua;
+            }
+        }
+    }
+
+    .accordion {
+        height: auto;
+        width: 100%;
+
+        &-item {
+            width: 100%;
+            border: none;
+            overflow: hidden;
+            outline: none;
+
+            .title {
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                cursor: pointer;
+                color: $gray;
+
+                .fa-icon.arrow {
+                    font-size: 0.9rem;
+                    color: $gray;
+                    transform: translateX(0) rotate(0);
+                    transition: transform 0.15s ease;
+                }
+            }
+
+            .accordion-contents {
+                width: 100%;
+                display: none;
+                color: $accent;
+                font-size: 0.8rem;
+                text-align: left;
+            }
+
+            &.active {
+                .title {
+                    color: $orange;
+                    .fa-icon {
+                        color: $orange;
+                        &.arrow {
+                            transform: translateX(0) rotate(180deg);
+                        }
+                    }
+                }
+
+                .accordion-contents {
+                    display: block;
+                }
             }
         }
     }
